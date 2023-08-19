5b692f97e12d58aeda7d60a9041f33bdbebbc8a6
muneeb-shahid <muneebshahid0102@gmail.com>
2023-08-19 16:58:57 +0500

    first commit

---
+++ lib/routes/approutes.dart
@@ -0,0 +1,35 @@
+import 'package:get/get.dart';
+
+
+class AppRoutes {
+  static appRoutes() => [
+        // GetPage(
+        //   name: RoutesName.WelcomeScreen,
+        //   page: () => WelcomeScreen(),
+        // ),
+        // GetPage(
+        //   name: RoutesName.LoginPage,
+        //   page: () => LoginPage(),
+        // ),
+        // GetPage(
+        //   name: RoutesName.SignUpPage,
+        //   page: () => SignUpPage(),
+        // ),
+        // GetPage(
+        //   name: RoutesName.BottomNav,
+        //   page: () => BottomNav(),
+        // ),
+      
+        // GetPage(
+        //   name: RoutesName.CreateBlog,
+        //   page: () => CreateBlog(),
+        // ),
+        // GetPage(
+        //   name: RoutesName.HomePage,
+        //   page: () => HomePage(),
+        // ),
+        // GetPage(name: RoutesName.Profile, page: () => Profile()),
+
+        
+      ];
+}
---
+++ lib/routes/routes_name.dart
@@ -0,0 +1,12 @@
+class RoutesName {
+  static String WelcomeScreen = '/';
+  static String LoginPage = '/LoginPage';
+  static String SignUpPage = "/SignUpPage";
+  static String BottomNav = "/BottomNav";
+  static String CreateBlog = "/CreateBlog";
+  static String HomePage = "/HomePage";
+  static String BlogFullPost = "/BlogFullPost";
+  static String Profile = "/Profile";
+
+  
+}
