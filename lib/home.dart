0dcdfd3947295f8935a4a2cb4a9d508af46a3328
muneeb-shahid <muneebshahid0102@gmail.com>
2023-08-20 21:25:29 +0500

    second commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class home extends StatelessWidget {
@@ -5,6 +6,35 @@ class home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    CollectionReference _products =
+        FirebaseFirestore.instance.collection('products');
+
+    return Scaffold(
+        body: SafeArea(
+          top: true,
+          child: StreamBuilder(
+              stream: _products.snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshots) {
+          if (streamSnapshots.hasData) {
+            return ListView.builder(
+              itemCount: streamSnapshots.data!.docs.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot documentSnapshot =
+                    streamSnapshots.data!.docs[index];
+                    return Card(
+                      child: ListTile(
+                        title: Text(documentSnapshot['name'],style: TextStyle(fontSize: 30,color: Colors.red),),
+                        subtitle: Text(documentSnapshot['price'].toString(),style: TextStyle(fontSize: 20,color: Colors.black),),
+        
+                      ),
+                    );
+              },
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+              },
+            ),
+        ));
   }
 }
