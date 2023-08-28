ec6fe674cf0b917d58c6a83796300210fcae6929
muneeb-shahid <muneebshahid0102@gmail.com>
2023-08-28 12:31:12 +0500

    twenty one commit

--- lib/controller/NoteController/NoteController.dart
+++ lib/controller/NoteController/NoteController.dart
@@ -1,3 +1,53 @@
 import 'package:get/get.dart';
-import 'package:flutter/material.dart';
-class NoteController extends GetxController{}
+import 'package:quill_html_editor/quill_html_editor.dart';
+
+class NoteController extends GetxController {
+  late QuillEditorController controller;
+  bool _hasFocus = false;
+  get hasFocus => _hasFocus;
+
+  @override
+  void onInit() {
+    super.onInit();
+    controller = QuillEditorController();
+    controller.onTextChanged((text) {
+      print('listening to $text');
+    });
+    controller.onEditorLoaded(() {
+      print('Editor Loaded :)');
+    });
+  }
+
+  @override
+  void onClose() {
+    controller.dispose();
+    super.onClose();
+  }
+
+  void getHtmlText() async {
+    String? htmlText = await controller.getText();
+    print(htmlText);
+  }
+
+  void setHtmlText(String text) async {
+    await controller.setText(text);
+  }
+
+  void insertNetworkImage(String url) async {
+    await controller.embedImage(url);
+  }
+
+  void insertVideoURL(String url) async {
+    await controller.embedVideo(url);
+  }
+
+  void insertHtmlText(String text, {int? index}) async {
+    await controller.insertText(text, index: index);
+  }
+
+  void clearEditor() => controller.clear();
+
+  void enableEditor(bool enable) => controller.enableEditor(enable);
+
+  void unFocusEditor() => controller.unFocus();
+}
